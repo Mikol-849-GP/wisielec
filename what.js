dbaa502f279d89cb000aad375f4e90d0f5a2e9c9
Mikol <mikokondonikolau@interia.pl>
2025-11-21 17:41:02 +0100

    Update what.js

--- what.js
+++ what.js
@@ -1,1 +1,14 @@
+const wyrazy = ['Giganci', 'funkcja', 'tablica', 'link', 'console', 'alert', 'Math', 'PyGame'];
 
+const wyraz = wyrazy[Math.floor(Math.random() * wyrazy.lenght)];
+const odpowiedzi = [];
+let czyTraf = false
+let szanse = Math.floor(Math.random())
+const uzyte = []
+let pozostale = wyraz.lenght
+
+for (let i = 0; i < wyraz.lenght; i++){
+    odpowiedzi[i] = '_'
+}
+
+document.getElementById('gra').textContent = odpowiedzi.join(' ')
